2abc492c65e10d9775711c552846e7a38f959a4a
Tobias Preuss <tobias.preuss@googlemail.com>
2014-02-08 20:55:45 +0100

    Initial import.

---
+++ README.md
@@ -0,0 +1,23 @@
+Random Rides Of Kindness
+========================
+
+A project startet at [hack4good 2014 in Berlin][1].
+
+How to run the thing?
+---------------------
+
+Please run a webserver to launch the website such as ...
+
+```
+$ python -m SimpleHTTPServer
+```
+
+Then open your browser and open ..
+
+```
+http://localhost:8000
+```
+
+
+
+[1]: https://geekli.st/hackathon/52c49d837689332d5f000019/project/52f544b5a5e91c1d009773a0?tab=about
---
+++ rrok.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+
+    var map = L.map('map').setView([52.516667, 13.383333], 10);
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+    }).addTo(map);
+
+    var popup = L.popup();
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+    map.on('click', onMapClick);
+
+    nameToColor = function(name) {
+        switch (name) {
+            case 'S-Bahnlinie S1': return "#DD4DAE";
+            case 'S-Bahnlinie S2': return "#108449";
+            case 'S-Bahnlinie S25': return "#108449";
+            case 'S-Bahnlinie S3': return "#166AB8";
+            case 'S-Bahnlinie S41': return "#A23F30";
+            case 'S-Bahnlinie S42': return "#BF8037";
+            case 'S-Bahnlinie S45': return "#BF8037";
+            case 'S-Bahnlinie S46': return "#BF8037";
+            case 'S-Bahnlinie S47: S Hermannstraße => S Spindlersfeld': return "#BF8037";
+            case 'S-Bahnlinie S5': return "#F36717";
+            case 'S-Bahnlinie S7': return "#7760B0";
+            case 'S-Bahnlinie S75': return "#7760B0";
+            case 'S-Bahnlinie S8': return "#55B831";
+            case 'S-Bahnlinie S85': return "#55B831";
+            case 'S-Bahnlinie S9': return "#942440";
+            default: throw "Cannot match name to color."
+        }
+    }
+
+    renderGeoJson = function(link) {
+        var url = $(link).attr("href");
+        $.getJSON(url, function(data) {
+            var feature = data.features[0];
+            var color = nameToColor(feature.properties.name);
+            var geojson = L.geoJson(data, {
+                style: {
+                    type: "LineString",
+                    color: color,
+                    weight: 3,
+                    opacity: 0.75
+                }
+            });
+            geojson.addTo(map);
+        });
+    }
+
+
+    // Load points GeoJSON and add to map
+    var links = $('link[rel="points"]');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        renderGeoJson(link);
+    }
+
+});
